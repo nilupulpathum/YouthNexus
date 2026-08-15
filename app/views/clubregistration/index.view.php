23a4a1db4e521baba64dc8ba465b2dd0a23d53aa
Nimesh Theekshana <theflippist49002@gmail.com>
2026-08-15 21:34:33 +0530

    refactor(views): remove external Google Fonts links in compliance with vanilla CSS constraint

--- app/views/clubregistration/index.view.php
+++ app/views/clubregistration/index.view.php
@@ -3,9 +3,6 @@
 <head>
     <meta charset="UTF-8">
     <title><?= htmlspecialchars($title) ?></title>
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Caveat:wght@600;700&family=Inter:wght@400;500;600;700;800&family=JetBrains+Mono:wght@500;600;700&family=Outfit:wght@500;600;700;800;900&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="<?= ROOT ?>/assets/css/dashboard.css?v=<?= time() ?>">
     <link rel="stylesheet" href="<?= ROOT ?>/assets/css/clubregistration.css?v=<?= time() ?>">
 </head>
