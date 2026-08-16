515193862c3b0470d3d96d8bbb3af54afcbf70f3
241168 <damikarajithuru@gmail.com>
2026-08-16 22:01:43 +0530

    task: integrate landing page from feat-landing and connect auth and registration navigation

--- app/controllers/Home.php
+++ app/controllers/Home.php
@@ -3,8 +3,17 @@
 
 class Home extends Controller {
 
-    public function index($a='', $b='', $c=''){
-        echo "This is home controller!";
-        $this->view('home');
+    public function index($a='', $b='', $c='') {
+        if (session_status() === PHP_SESSION_NONE) {
+            session_start();
+        }
+
+        $data = [
+            'isLoggedIn' => isset($_SESSION['user_id']),
+            'userName'   => $_SESSION['user_name'] ?? '',
+            'userRole'   => $_SESSION['user_role'] ?? '',
+        ];
+
+        $this->view('home', $data);
     }
 }
